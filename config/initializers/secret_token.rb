73c7a8436cb6873899a5fe94a93a2c3ced143d6e
Johnny Rodgers <johnny.rodgers@gmail.com>
2014-04-09 20:29:01 -0700

    Rails scaffolding

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SlackMap::Application.config.secret_token = 'b3b157550befbb27d5d6697af94a845b7ecd19a7212612dd3d8f64c4dd45da46087503a30ea25a2de0612e3f4a7ea9f7d5908a50e88fabbc92c5dfcad12cf0aa'
